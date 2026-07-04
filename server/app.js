80d88841be2f3ca8efc10d912b6fe56c63204128
Hussein AbuMariam <huseinmohamaad@gmail.com>
2026-07-04 15:48:48 +0300

    cookies-ws

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { join } = require("path");
-
+const cookieParser = require("cookie-parser");
 const router = require("./controllers");
 
 const app = express();
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(join(__dirname, "..", "public")));
-
+app.use(cookieParser());
 app.set("port", process.env.PORT || 3000);
 
 app.use(router);
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -5,8 +5,27 @@ const error = require("./error");
 const router = express.Router();
 
 // write your code
+const COOKIE_NAME = "auth";
+router.post("/login", (req, res) => {
+    res.cookie(COOKIE_NAME, "logged-in", {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60
+    })
+    res.redirect("/");
+});
+router.post("/logout", (req, res) => {
+    res.clearCookie(COOKIE_NAME);
+    res.redirect("/");
+});
+router.get("/auth-check", (req, res) => {
+  const cookie = req.cookies && req.cookies[COOKIE_NAME];
 
+  if (cookie === "logged-in") {
+    return res.status(200).send("User is authenticated");
+  }
 
+  return res.status(401).send("User is NOT authenticated");
+});
 router.use(error.client);
 
 module.exports = router;
